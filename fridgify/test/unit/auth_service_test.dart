ece5c1082a1648e5fa5eb3a1ab0a812a1075a6fa
JoschuaGoetz <joschua.goetz@gmail.com>
2020-04-14 00:41:56 +0200

    Add tests for login

--- fridgify/test/unit/auth_service_test.dart
+++ fridgify/test/unit/auth_service_test.dart
@@ -1,14 +1,12 @@
 import 'dart:convert';
 
-import 'package:flutter/foundation.dart';
-import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fridgify/data/repository.dart';
 import 'package:fridgify/exception/failed_to_fetch_api_token_exception.dart';
 import 'package:fridgify/exception/failed_to_fetch_client_token.dart';
+import 'package:fridgify/model/user.dart';
 import 'package:fridgify/service/auth_service.dart';
-import 'package:fridgify/utils/validator.dart';
-import 'package:http/http.dart' show Client, Response;
+import 'package:http/http.dart' show Response, Request;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/testing.dart';
 
@@ -22,21 +20,53 @@ void main() async {
     Repository.sharedPreferences = await SharedPreferences.getInstance();
 
     mockClient = new MockClient((request) async {
-      switch(request.headers.remove('Authorization')) {
-        case 'Api returns no valid token':
-          return Response('Api returns no valid token', 404);
-        case 'Api returned valid token':
-          final responseBody = {
-            'token': 'Api token',
-            'validation_time': 42069,
-          };
-          return Response(json.encode(responseBody), 201);
+      var handler = ResponseHandlers();
+      var endpoint = request.url.toString().replaceAll('${Repository.baseURL}auth/', '');
+
+      switch(endpoint) {
+        case "/register/":
+          return handler.handleRegisterRequest(request);
+        case '/token/':
+          return handler.handleFetchApiTokenRequest(request);
+        case '/login/':
+          return handler.handleLoginRequest(request);
         default:
           return Response('Not implemented', 201);
       }
     });
 
     authService = AuthenticationService(mockClient);
+    authService.user = User.newUser(
+        username: 'Mr. Mock',
+        password: 'secret',
+        name: 'Dieter',
+        surname: 'Mock',
+        email: 'dieter.mock@miau.de',
+        birthDate: '01.01.1969'
+    );
+  });
+
+  group('Login', () {
+
+    test('gets a return type other than 201', () async {
+      authService.user.username = 'error';
+      expect(() async => completion(await authService.login()), throwsA(predicate((error) => error is FailedToFetchClientTokenException && error.err == 'Error. Login failed')));
+      authService.user.username = 'Mr. Mock';
+    });
+
+    test('gets a api token', () async {
+      expect(Future.value('Api token'), completion(await authService.login()));
+      expect(Future.value('Api token'), completion(await Repository.sharedPreferences.get('clientToken')));
+    });
+    
+  });
+
+  group('Register', () {
+
+    test('gets a return type other than 201', () async {
+      expect(() async => completion(await authService.register()), throwsA(predicate((error) => error is FailedToFetchClientTokenException)));
+    });
+
   });
 
   group('Logout', () {
@@ -69,18 +99,52 @@ void main() async {
     });
 
     test('gets a return type other than 201', () async {
-      await Repository.sharedPreferences.setString('clientToken', 'Api returns no valid token');
+      await Repository.sharedPreferences.setString('clientToken', 'Other than 201');
 
       expect(() async => completion(await authService.fetchApiToken()), throwsA(predicate((error) => error is FailedToFetchApiTokenException)));
     });
 
     test('gets a token and sets it', () async {
-      await Repository.sharedPreferences.setString('clientToken', 'Api returned valid token');
+      await Repository.sharedPreferences.setString('clientToken', 'valid token');
 
       expect(Future.value('Api token'), completion(await authService.fetchApiToken()));
       expect(Future.value('Api token'), completion(await Repository.sharedPreferences.get('apiToken')));
     });
 
   });
+
+}
+
+class ResponseHandlers {
+
+  ResponseHandlers();
+
+  Response handleRegisterRequest(Request request) {
+    return Response('No register', 402);
+  }
+
+  Response handleLoginRequest(Request request) {
+    var name = json.decode(request.body)['username'];
+
+    if (name == 'error') {
+      return Response(json.encode({'detail': 'Error. Login failed'}), 401);
+    }
+    return Response(json.encode({'token': 'Api token'}), 200);
+  }
+
+  Response handleFetchApiTokenRequest(Request request) {
+    switch(request.headers.remove('Authorization')){
+      case 'valid token':
+        final responseBody = {
+          'token': 'Api token',
+          'validation_time': 42069,
+        };
+        return Response(json.encode(responseBody), 201);
+      case 'Other than 201':
+        return Response('Other than 201', 404);
+      default:
+        return Response('Not implemented', 500);
+    }
+  }
 
 }

ba10b557bacdd5516bcdd3a84120e372cb28f528
JoschuaGoetz <joschua.goetz@gmail.com>
2020-05-24 14:11:43 +0200

    Add test for withdraw in contentRepository

--- fridgify/test/unit/content_repository_test.dart
+++ fridgify/test/unit/content_repository_test.dart
@@ -41,6 +41,8 @@ void main() async {
           return testUtil.handleFetchAllRequest(request);
         case 'Update':
           return testUtil.handleUpdateRequest(request);
+        case 'Withdraw':
+          return testUtil.handleWithdrawRequest(request);
         default:
           return Response(data: 'Not implemented', statusCode: 201);
       }
@@ -158,12 +160,19 @@ void main() async {
 
   group('group', () {
     setUp(() {
+      contentRepository.contents.clear();
       //Creates 9 copies of three individual items
+      //Uses modulo 3 to create items with the same name. Use a multiple of
+      //three to have the same amount of items in each group
       var content = testUtil.createContent(27);
       content.forEach((entry) {
         contentRepository.contents[entry.contentId] = entry;
       });
-
+    
+    });
+    
+    tearDown(() {
+      contentRepository.contents.clear();
     });
 
     test('groups 3 items', () {
@@ -171,6 +180,7 @@ void main() async {
 
       var groupedContent = contentRepository.grouped;
 
+      print((groupedContent));
       expect(groupedContent.length, 3);
 
       for(int i = 0; i < 3; i++) {
@@ -178,6 +188,68 @@ void main() async {
       }
     });
 
+  });
+
+  group('withdraw', () {
+    setUp(() {
+      testUtil.setTestCase('Withdraw');
+    });
+
+    test('throws an error', () async {
+      testUtil.setId('Error case withdraw');
+
+      expect(
+              () async => completion(
+              await contentRepository.withdraw(content, 2)),
+          throwsA(
+              predicate((error) => error is FailedToFetchContentException)));
+    });
+
+    test('withdraws successfully', () async {
+      testUtil.setId('Withdraw with something left');
+
+      var testContent = Content.create(
+        contentId: "uuid123141141",
+        expirationDate: DateTime.now().toIso8601String(),
+        count: 1,
+        amount: 20,
+        unit: 'g',
+        item: Item(
+            itemId: 5165,
+            barcode: 'adw',
+            name: 'Human heart',
+            store: Store.create(name: 'Ikea')),
+      );
+
+      Content content = await contentRepository.withdraw(testContent, 10);
+
+      expect(contentRepository.contents.containsKey('uuid123141141'), true);
+
+    });
+
+    test('removes the item', () async {
+      testUtil.setId('Withdraw without something left');
+
+      var testContent = Content.create(
+        contentId: "uuid123",
+        expirationDate: DateTime.now().toIso8601String(),
+        count: 1,
+        amount: 20,
+        unit: 'g',
+        item: Item(
+            itemId: 51653,
+            barcode: 'adw',
+            name: 'Human heart',
+            store: Store.create(name: 'Ikea')),
+      );
+      contentRepository.contents[testContent.contentId] = testContent;
+      testContent.amount = 0;
+
+      Content content = await contentRepository.withdraw(testContent, 20);
+
+      expect(contentRepository.contents.containsKey('uuid123'), false);
+
+    });
   });
 
   group('update', () {
@@ -210,6 +282,19 @@ class ContentRepositoryTestUtil extends TestUtil {
   ContentRepositoryTestUtil(Dio dio) : super(dio);
 
   Map body;
+
+  Response handleWithdrawRequest(RequestOptions request) {
+    switch (request.extra['id']) {
+      case 'Error case add content':
+        return Response(data: 'Error case withdraw', statusCode: 404);
+      case 'Withdraw with something left':
+        return Response(data: 'Item response that is not used', statusCode: 200);
+      case 'Withdraw without something left':
+        return Response(data: 'Item response that is not used', statusCode: 200);
+      default:
+        return Response(data: 'Not implemented', statusCode: 500);
+    }
+  }
 
   Response handleAddRequest(RequestOptions request) {
     switch (request.extra['id']) {

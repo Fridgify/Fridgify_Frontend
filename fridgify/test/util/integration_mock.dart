474e47cd017dc09ebfd65979441d56b7da34df70
JoschuaGoetz <joschua.goetz@gmail.com>
2020-05-31 11:06:46 +0200

    Mock used class for login test

--- fridgify/test/util/integration_mock.dart
+++ fridgify/test/util/integration_mock.dart
@@ -1,18 +1,22 @@
 import 'package:fridgify/service/auth_service.dart';
 import 'package:mockito/mockito.dart';
 
+class MockAuthenticationService extends Mock implements AuthenticationService {}
+
 class IntegrationMock {
-  static AuthenticationService authService = AuthenticationService();
+  static MockAuthenticationService mockAuthService =
+      MockAuthenticationService();
 
   static void register() {
-    when(authService.register()).thenAnswer((_) => Future.value('token'));
-    when(authService.login()).thenAnswer((_) => Future.value('token'));
+    when(mockAuthService.register()).thenAnswer((_) => Future.value('token'));
+    when(mockAuthService.login()).thenAnswer((_) => Future.value('token'));
   }
 
   static void login() {
-    when(authService.login()).thenAnswer((_) => Future.value('token'));
-    when(authService.fetchApiToken()).thenAnswer((_) => Future.value('token'));
-    when(authService.initiateRepositories())
+    when(mockAuthService.login()).thenAnswer((_) => Future.value('token'));
+    when(mockAuthService.fetchApiToken())
+        .thenAnswer((_) => Future.value('token'));
+    when(mockAuthService.initiateRepositories())
         .thenAnswer((_) => Future.value(true));
   }
 }
